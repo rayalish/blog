222518c8bcb494e1e77bb142cbc9016eeca9f462
Alish <a_alisherrr@mail.ru>
2024-01-19 15:54:26 +0600

    with server settings

---
+++ blog/blog/prod_settings.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-+_(cr#3654v5d_j4!$hx+n0w75b7&7()0l*dsfvhjfdsvhj'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '64.226.78.71']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ blog/config/gunicorn.conf.py
@@ -0,0 +1,5 @@
+bind = '127.0.0.1:8000'
+workers = 2
+user = 'tebirenn'
+timeout = 120 # seconds
+wsgi_app = '/home/tebirenn/kino_6/kinopoisk/kinopoisk/wsgi:application'
