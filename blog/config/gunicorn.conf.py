9eddb3652226daeb462fbd2be804d923b5b01008
Alish <a_alisherrr@mail.ru>
2024-01-20 21:27:56 +0600

    with new ways

--- blog/config/gunicorn.conf.py
+++ blog/config/gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = '127.0.0.1:8000'
 workers = 2
-user = 'tebirenn'
+user = 'rayalish'
 timeout = 120 # seconds
-wsgi_app = '/home/tebirenn/kino_6/kinopoisk/kinopoisk/wsgi:application'
+wsgi_app = '/home/rayalish/blog/blog/blog/wsgi:application'
